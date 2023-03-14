c6be7cf6af3ef58e06083f277eb8a3088c377341
Vedant Goradiya <vedantgoradiya1998@gmail.com>
2023-03-14 15:57:03 -0400

    First commit to budget app build using React (Context API)

---
+++ src/components/Budget.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react';
+import EditBudget from './EditBudget';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+	const { budget, dispatch } = useContext(AppContext);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
+
+	const handleSaveClick = (value) => {
+		dispatch({
+			type: 'SET_BUDGET',
+			payload: value,
+		});
+		setIsEditing(false);
+	};
+
+	return (
+		<div class='alert alert-primary p-3 d-flex align-items-center justify-content-between'>
+			{isEditing ? (
+				<EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+			) : (
+				<>
+					<span>Budget: $ {budget}</span>
+					<button type='button' class='btn btn-primary' onClick={handleEditClick}>
+						Edit
+					</button>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Budget;
